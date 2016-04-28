7f49a648cb89594bead22cc0e6e47b9dee0af521
Mariah Tree <mariah.tree@gmail.com>
2016-04-28 14:01:37 -0500

    initalize repo and add boilerplate files

---
+++ js/main.js
@@ -0,0 +1,7 @@
+(function() {
+  // All code NOT referencing DOM elements can go here
+  document.addEventListener("DOMContentLoaded", function(e){
+    // ALL DOM RELATED QUERYING GOES HERE
+  });
+
+}());
